eae5e389398429ed34e422e0a49ff51638fa4c09
kornilov-mr <kornilov.mr@gmail.com>
2024-02-24 17:09:51 +0100

    added NastedRangeCheck and Count

--- sorting/NestedRangeCount.cpp
+++ sorting/NestedRangeCount.cpp
@@ -2,6 +2,9 @@
 #include<math.h>
 #include <set>
 #include<iostream>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
 
 struct range{
@@ -11,7 +14,12 @@ struct range{
 };
 bool compareByLengthLess(const range &f, const range &s)
 {
+    if(f.a == s.a) return f.b < s.b;
     return f.a < s.a;
+}
+bool compareByLengthLessPair(const pair<int,int> &f, const pair<int,int> &s)
+{
+    return f.first < s.first;
 }
 bool compareByLengthBigger(const range &f, const range &s)
 {
@@ -20,6 +28,7 @@ bool compareByLengthBigger(const range &f, const range &s)
 int main(){
     int n;
     cin>>n;
+    multiset<int> bounders;
     vector<range> ranges;
     vector<int> contains;
     vector<int> contained;
@@ -31,30 +40,24 @@ int main(){
         contained.push_back(0);
     }
     std::sort(ranges.begin(), ranges.end(),compareByLengthLess);
-    int max=0;
     for(int i=0;i<n;i++){
-        if(max>=ranges[i].b){
-                contains[ranges[i].i]=1;
-        }else{
-            max=ranges[i].b;
-        }
+        auto it= bounders.lower_bound(ranges[i].b);
+        contained[ranges[i].i]=i-distance(bounders.begin(), it);
+        bounders.insert(ranges[i].b);
     }
-    std::sort(ranges.begin(), ranges.end(),compareByLengthBigger);
-    int min=1000000001;
+    bounders.clear();
+    for(int i=n-1;i>=0;i--){
+        auto it=bounders.upper_bound(ranges[i].b);
+        contains[ranges[i].i]=distance(bounders.begin(), it);
+        bounders.insert(ranges[i].b);
+    }
 
     for(int i=0;i<n;i++){
-        if(min<=ranges[i].b){
-            contained[ranges[i].i]=1;
-        }else{
-            min=ranges[i].b;
-        }
-    }
-    for(int i=0;i<n;i++){
-        cout<<contained[i]<<" ";
+        cout<<contains[i]<<" ";
     }
     cout<<endl;
     for(int i=0;i<n;i++){
-        cout<<contains[i]<<" ";
+        cout<<contained[i]<<" ";
     }
 
 }
