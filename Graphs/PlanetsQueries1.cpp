d8112f0d5a8028b8521f2e953fa3299f19c0e60f
kornilov-mr <kornilov.mr@gmail.com>
2024-03-20 19:20:56 +0100

    added PlanetsQueries1.cpp

---
+++ Graphs/PlanetsQueries1.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#include <queue>
+using namespace std;
+
+
+/* Data Type  */
+#define ll long long
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+/* Functions */
+/* Loop */
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+
+#define pb push_back
+#define eb emplace_back
+
+/* Printing vector */
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+
+/* Utility */
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+const ll mod = 1e9 + 7;
+
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+
+
+const int MAXN = 2e5 + 5;
+const int MAXD = 30;  // ceil(log2(10^9))
+
+// number of planets and queries
+int n, q;
+// parent matrix where [i][j] corresponds to i's (2^j)th parent
+int parent[MAXN][MAXD];
+
+int jump(int a, int d) {
+    for (int i = 0; i < MAXD; i++)
+        if (d & (1 << i)) a = parent[a][i];
+    return a;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> q;
+    for (int i = 1; i <= n; i++) { cin >> parent[i][0]; }
+    // evaluate the parent matrix
+    for (int d = 1; d < MAXD; d++)
+        for (int i = 1; i <= n; i++) {
+            parent[i][d] = parent[parent[i][d - 1]][d - 1];
+        }
+    // process queries
+    while (q--) {
+        int a, d;
+        cin >> a >> d;
+        cout << jump(a, d) << '\n';
+    }
+}
